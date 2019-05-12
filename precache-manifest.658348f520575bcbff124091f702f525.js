2b0b5795aa414ffaddca790eda2b40e7576e1ca4
Timinis <litimc3@gmail.com>
2019-05-11 21:50:19 -0700

    Updates

---
+++ precache-manifest.658348f520575bcbff124091f702f525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74ad87284ef6c8d5de0f3be90f98f74",
+    "url": "/Dictionary-Trie-D3/index.html"
+  },
+  {
+    "revision": "20e145a6287ef6bdf293",
+    "url": "/Dictionary-Trie-D3/static/css/main.9b1fad16.chunk.css"
+  },
+  {
+    "revision": "96240220ef99041b71c6",
+    "url": "/Dictionary-Trie-D3/static/js/2.6aaa61b9.chunk.js"
+  },
+  {
+    "revision": "20e145a6287ef6bdf293",
+    "url": "/Dictionary-Trie-D3/static/js/main.f74f0781.chunk.js"
+  },
+  {
+    "revision": "cb2542e8c361b15e29f0",
+    "url": "/Dictionary-Trie-D3/static/js/runtime~main.25e4b857.js"
+  },
+  {
+    "revision": "871ac98d0749fad5f245f2c95d2c373f",
+    "url": "/Dictionary-Trie-D3/static/media/edge.871ac98d.jpg"
+  },
+  {
+    "revision": "551d931542400e6cfcd276f60fc4f2dd",
+    "url": "/Dictionary-Trie-D3/static/media/node.551d9315.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Dictionary-Trie-D3/precache-manifest.d61c512da37ec40f64ef7e9402bbabfb.js"
+  "/Dictionary-Trie-D3/precache-manifest.658348f520575bcbff124091f702f525.js"
 );
 
 self.addEventListener('message', (event) => {
